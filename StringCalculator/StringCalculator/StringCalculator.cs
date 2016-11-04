7b0a0996406770b6a0b4561c6ee1ed5fe4609daa
John Jackson <john.jackson@advancedcomputersoftware.com>
2016-11-04 11:24:46 +0000

    initial commit

---
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        private const string MINUS = "minus";
+        private Dictionary<string, double> _stringToNum = new Dictionary<string, double>
+        {
+            {"zero", 0},
+            {"one", 1},
+            {"two", 2},
+            {"three", 3 },
+            {"four", 4 },
+            {"five", 5 },
+            {"six", 6 },
+            {"twelve", 12 },
+            {"nineteen", 19 },
+            {"twenty", 20 },
+            {"thirty", 30 },
+            {"seventy", 70 },
+            {"hundred", 100 },
+            {"thousand", 1000 },
+            {"million", 1000000 }
+        };
+
+        public double Add(string number1, string number2)
+        {
+
+            var intNum1 = ParseNumber(number1);
+            var intNum2 = ParseNumber(number2);
+
+            return intNum1 + intNum2;
+        }
+
+        private double ParseNumber(string numbersString)
+        {
+            double result;
+
+            numbersString = numbersString.ToLower();
+            numbersString = numbersString.Replace('-', ' ');
+
+            var numbers = ParseString(numbersString);
+
+
+            for (var i =0; i < numbers.Count; i++)
+            {
+                if (!(numbers[i] >= 100)) continue;
+
+                numbers[i] = numbers[i]*numbers[i - 1];
+                numbers.RemoveAt(i -1);
+            }
+
+            return double.TryParse(numbersString, out result) ? result : numbers.Sum();
+        }
+
+        private List<double> ParseString(string numbersString)
+        {
+            var numbers = new List<double>();
+            var negativeAll = numbersString.Contains(MINUS);
+
+            foreach (var numberString in numbersString.Split(' '))
+            {
+                if (!_stringToNum.ContainsKey(numberString)) continue;
+
+                if (!negativeAll)
+                    numbers.Add(_stringToNum[numberString]);
+                else
+                    numbers.Add(_stringToNum[numberString] * -1);
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ StringCalculator/stringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        private StringCalculator _stringCalculator;
+
+        [OneTimeSetUp]
+        public void GivenAStringCalculator()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [TestCase("0", "0", 0)]
+        [TestCase("1", "0", 1)]
+        [TestCase("one", "0", 1)]
+        [TestCase("one", "two", 3)]
+        [TestCase("five", "zero", 5)]
+        [TestCase("five", "twelve", 17)]
+        [TestCase("nineteen","twenty",39)]
+        [TestCase("one", "twenty-One", 22)]
+        [TestCase("4294967295", "0", 4294967295)]
+        [TestCase("4294967295", "1", 4294967296)]
+        [TestCase("one hundred and six", "1", 107)]
+        [TestCase("minus two", "1", -1)]
+        [TestCase("minus two", "five million six hundred and thirty thousand four hundred and seventy-three", 5630471)]
+        public void WhenAddIsCalled(string number1, string number2, double expectedResponse)
+        {
+            var response = _stringCalculator.Add(number1, number2);
+            Assert.That(response, Is.EqualTo(expectedResponse));
+        }
+    }
+}
